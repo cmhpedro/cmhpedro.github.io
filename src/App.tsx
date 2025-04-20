2981210ae526f6ee567371998099867169f41c34
Pedro Mariano <ph_marianocorrea@hotmail.com>
2025-04-20 13:18:53 -0300

    feat: use hash router instead of browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css'
 
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 
 import { TCC } from './pages/TCC'
 
 export function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<TCC />} />
         <Route path="/tcc" element={<TCC />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
